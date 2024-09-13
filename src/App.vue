31355b087b587b02774fc64a1d9954c75f64c7f1
long <longbao7552@gmail.com>
2024-09-12 17:19:37 -0700

    chenggong

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
     <!-- 5-2配置导航，配置路由出口，路径匹配的路由显示的位置 -->
     <div class="luyoukuang">
       <div class="luyou">
-        <router-link to="/find">发现音乐</router-link>
-        <router-link to="/my">我的音乐</router-link>
-        <router-link to="/friend">朋友</router-link>
+        <router-link to="/find">单词列表</router-link>
+        <router-link to="/my">美剧收藏</router-link>
+        <router-link to="/friend">我的数据</router-link>
       </div>
       <div class="top">
-        <router-view></router-view>
+        <keep-alive :include="findmeEnglind">
+          <router-view></router-view>
+        </keep-alive>
       </div>
     </div>
 
@@ -32,6 +34,11 @@ import day_5 from "./components/day_5/day_5.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      findmeEnglind: ["findme"],
+    };
+  },
   components: {
     // HelloWorld
     day_4,
@@ -54,31 +61,41 @@ h3 {
   text-align: center;
 }
 .luyoukuang {
-  border: solid 1px black;
+  border: solid 5px rgb(37, 35, 47);
   margin: 0 10px;
+  border-radius: 20px;
+  padding: 10px;
 }
 .luyou {
   /* border: solid 1px black; */
   display: flex;
   justify-content: space-around;
+  border-radius: 20px;
+  background-color: rgb(37, 35, 47);
 }
 .luyou a {
   height: 40px;
   flex: 1;
-  border-right: solid 1px rgb(141, 141, 141);
+  /* border-right: solid 1px rgb(141, 141, 141); */
   text-align: center;
   text-decoration: none;
   line-height: 40px;
   background-color: rgb(37, 35, 47);
   color: white;
+  border-radius: 20px;
+  font-weight: 200;
 }
 .luyou a:last-child {
-  border-right: none;
+  /* border-right: none; */
 }
 .luyou a:hover {
   background-color: rgb(87, 85, 95);
 }
 .luyou a.router-link-active {
-  background-color: rgb(87, 63, 184);
+  background-color: rgb(125, 125, 126);
+}
+.top {
+  height: 500px;
+  overflow: scroll;
 }
 </style>
--- src/views/Find.vue
+++ src/views/Find.vue
@@ -1,17 +1,193 @@
 <template>
-  <div>
-    <p>vue 学习</p>
-    <p>vue 学习</p>
-    <p>vue 学习</p>
+  <div class="P_Find">
+    <!-- <p>单词列表</p> -->
+    <div class="block" v-for="(item, index) in title" :key="item.id">
+      <div class="biaoti">
+        <div>
+          <span>{{ index + 1 }}.</span><span>{{ item.name }}</span>
+        </div>
+        <button class="quxiao" @click="shanchu(item.id)">X</button>
+      </div>
+      <div class="words_part">
+        <div class="words" v-for="(item, index) in item.words" :key="item.id">
+          <ul>
+            <li>
+              <span>{{ index + 1 }}-</span>
+              <span>{{ item.name }}</span>
+              <span>{{ item.translate }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <button class="add_word" @click="add_words(item.id)">添加新单词</button>
+    </div>
+    <div class="block_add"><button @click="add">+</button></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "findme",
+  data() {
+    return {
+      title: JSON.parse(localStorage.getItem("WordsLists")) || [
+        {
+          id: "1",
+          name: "动物类",
+          words: [{ id: "0", name: "dog", translate: "狗" }],
+        },
+        {
+          id: "2",
+          name: "人类",
+          words: [
+            { id: "1", name: "human", translate: "人类" },
+            { id: "2", name: "women", translate: "女人" },
+          ],
+        },
+      ],
+      words: {},
+    };
+  },
+  methods: {
+    add() {
+      const titlee = prompt("请输入分类");
+      if (titlee) {
+        const new_titile = {
+          id: this.title.length + 1,
+          name: titlee,
+          words: [],
+        };
+        this.title.push(new_titile);
+      }
+      console.log(this.title);
+      localStorage.setItem("WordsLists", JSON.stringify(this.title));
+    },
+    add_words(id) {
+      console.log(id);
+      const new_words = prompt("请输入单词");
+      const new_trans = prompt("请输入释义");
+      if (new_words && new_trans) {
+        const newwords = {
+          id: this.words.length + 1,
+          name: new_words,
+          translate: new_trans,
+        };
+        this.title[id - 1].words.push(newwords);
+      }
+      localStorage.setItem("WordsLists", JSON.stringify(this.title));
+    },
+    shanchu(id) {
+      this.title = this.title.filter((item) => item.id !== id);
+      localStorage.setItem("WordsLists", JSON.stringify(this.title));
+    },
+  },
+};
 </script>
 
 <style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.P_Find {
+  /* border: solid 1px black; */
+  /* padding: 20px 0; */
+  /* width: 500px; */
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 p {
   text-align: center;
+}
+.words_part {
+  /* border: solid 1px black; */
+  padding: 5px 0;
+}
+
+.block,
+.block_add {
+  /* flex: none; */
+  display: inline-block;
+  width: 200px;
+  height: 150px;
+  border: solid 2px black;
+  margin: 5px;
+  border-radius: 10px;
+  box-shadow: 0 3px 10px 0.6px rgba(193, 193, 193, 0.8);
+}
+.block {
+  /* border: solid 1px black; */
+  /* padding: 5px; */
+  position: relative;
+  overflow: hidden;
+}
+.biaoti {
+  height: 30px;
+  line-height: 30px;
+  padding: 0 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(255, 183, 80);
+
+  /* color: white; */
+}
+.biaoti span {
+  font-weight: 400;
+}
+.words {
+  padding: 0 5px;
+}
+.words ul {
+  list-style: none;
+}
+.words ul li {
+  border-bottom: solid 1px rgb(156, 156, 156);
+}
+.words ul li:hover {
+  cursor: pointer;
+  background-color: rgb(255, 248, 238);
+}
+.block_add {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.block_add button {
+  width: 100%;
+  height: 100%;
+  font-size: 30px;
+  border-radius: 10px;
+  cursor: pointer;
+  border: 0;
+  color: white;
+  font-size: 150px;
+  line-height: 50px;
+  background-color: rgb(255, 183, 80);
+}
+.block_add button:hover {
+  background-color: rgb(255, 220, 170);
+}
+.add_word {
+  width: 100%;
+  height: 30px;
+  position: absolute;
+  bottom: 0px;
+  cursor: pointer;
+  border-radius: 0 0 10px 10px;
+  border: 0;
+  color: white;
+  background-color: rgb(218, 218, 218);
+}
+.add_word:hover {
+  background-color: rgb(255, 220, 170);
+}
+.quxiao {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 }
 </style>
